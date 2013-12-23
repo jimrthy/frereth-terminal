2f359c440f83bd12c7d4d56eedfc822a0b1c14a7
James Gatannah <james.gatannah@gmail.com>
2013-12-22 22:03:42 -0600

    Initial project generation

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to frereth-terminal
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/frereth_terminal/core_test.clj
@@ -0,0 +1,7 @@
+(ns frereth-terminal.core-test
+  (:use clojure.test
+        frereth-terminal.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

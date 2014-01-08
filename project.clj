b6af8e38e114842195fd952c07cbbec36c5cffb5
James Gatannah <james.gatannah@gmail.com>
2014-01-07 20:48:45 -0600

    Toss up a do-nothing window on system (reset). It throws an unhandled exception on any input, but it's a start

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject frereth-terminal "0.1.0-SNAPSHOT"
   :description "UI's more important than eye candy"
-  :url "http://example.com/FIXME"
+  :url "http://www.frereth.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
--- src/frereth_terminal/system.clj
+++ src/frereth_terminal/system.clj
@@ -1,15 +1,66 @@
 (ns frereth-terminal.system
+  (:require [penumbra.app :as app]
+            [penumbra.text :as text])
   (:gen-class))
+
+(set! *warn-on-reflection* true)
 
 (defn init
   "Empty system to build upon later"
   []
-  {})
+  {:text ""
+   ;; TODO: Start with current system time?
+   :time 0})
+
+(defn close [state]
+  (println "Closing")
+  (println state))
+
+(defn update [[delta time] state]
+  ;; The important thing should be that this updates the state so
+  ;; that...what can draw it?
+  (into state {:time time}))
+
+(defn display
+  "I'm pretty sure this is supposed to actually draw the state that
+update set up."
+  [[dt t] state]
+  (text/write-to-screen (:text state) 0 0)
+  (app/repaint!))
+
+(defn restore-last-session
+  "For now, at least, set up a Penumbra window.
+Hopefully this will get more robust in the future.
+Session restoration gets complicated."
+  [dead]
+  (into dead
+        {:width 1024
+         :height 768
+         :title "Frerminal"
+         :text ""}))
+
+(defn configure-window
+  [state]
+  (app/title! "frerepl")
+  (app/vsync! true))
+
+(defn key-press [key state]
+  (cond
+   (= :escape key) (app/stop!)
+   :else (into state (str (:text state) key))))
 
 (defn start
   "Run all the side-effects associated with starting a system"
   [dead]
-  dead)
+  (let [prev (restore-last-session dead)]
+    (app/start
+     {:init configure-window
+      :update update
+      :display display
+      :key-press key-press
+      :close close}
+     prev)
+    prev))
 
 (defn stop
   "Run all the side-effects to kill a system off"

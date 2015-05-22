90be2fca462f4ccb189d2285a5dc75b47b835284
James Gatannah <james.gatannah@gmail.com>
2015-05-21 22:18:04 -0500

    Bump ancient versions
    Esp. clojure

--- project.clj
+++ project.clj
@@ -5,14 +5,14 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[com.frereth/penumbra "0.6.7-SNAPSHOT"]
                  [im.chit/ribol "0.4.0"]
-                 [org.clojure/clojure "1.7.0-alpha5"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+                 [org.clojure/clojure "1.7.0-RC1"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [prismatic/schema "0.4.2"]]
   :jvm-opts [~(str "-Djava.library.path=/usr/local/lib:"
                    (System/getProperty "java.library.path"))]
   :main frereth-terminal.core
   :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[com.stuartsierra/component "0.2.2"]
-                                  [org.clojure/tools.namespace "0.2.9"]
-                                  [org.clojure/java.classpath "0.2.2"]
-                                  [prismatic/schema "0.3.7"]]}}
+                   :dependencies [[com.stuartsierra/component "0.2.3"]
+                                  [org.clojure/tools.namespace "0.2.10"]
+                                  [org.clojure/java.classpath "0.2.2"]]}}
   :repl-options {:init-ns user})
